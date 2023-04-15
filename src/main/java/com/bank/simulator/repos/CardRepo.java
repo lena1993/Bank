ab9091aa446deae017182c03e33144b1f3587228
Admin <lsargsyan1993@gmail.com>
2023-04-16 00:40:22 +0400

    Adding findByPan function in CardRepo

--- src/main/java/com/bank/simulator/repos/CardRepo.java
+++ src/main/java/com/bank/simulator/repos/CardRepo.java
@@ -3,5 +3,8 @@ package com.bank.simulator.repos;
 import com.bank.simulator.domain.Cards;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CardRepo extends CrudRepository<Cards, Integer> {
+    Cards findByPan(String pan);
 }
