80195feba8c84c13c955e0663f5723870c45161a
Admin <lsargsyan1993@gmail.com>
2023-04-22 20:20:42 +0400

    Creating checkPinOrFingerprint method

--- src/main/java/com/bank/simulator/service/BankService.java
+++ src/main/java/com/bank/simulator/service/BankService.java
@@ -7,8 +7,8 @@ import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.bank.simulator.ApplicationProperties;
 import com.bank.simulator.domain.ValidatedCardStorage;
-import com.bank.simulator.CardAuthentication;
 import com.bank.simulator.domain.Cards;
+import com.bank.simulator.model.AuthType;
 import com.bank.simulator.repos.CardRepo;
 import com.bank.simulator.model.Card;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +22,14 @@ import java.util.*;
 
 @Service
 public class BankService {
+    private final String PIN = "PIN";
+    private final String CARD_TOKEN = "cardToken";
+    private final String FINGERPRINT = "FINGERPRINT";
+    private final String CARD = "card";
+    private final String CARD_UPPERCASE = "Card";
+    private final String PAN = "pan";
+    private final String CARD_DETAILS = "Card Details";
 
-
-    @Autowired
-    private CardAuthentication cardAuthentication;
 
     @Autowired
     private ValidatedCardStorage validatedCardStorage;
@@ -37,7 +41,6 @@ public class BankService {
     ApplicationProperties applicationProperties;
 
     public ResponseEntity checkCardExistence(Card insertedCard) {
-
         Cards card = cardRepo.findByPan(insertedCard.getPan());
 
         if(card.getCardHolder().equals(insertedCard.getCardHolder()) && card.getExpiryDate().equals(insertedCard.getExpiryDate())
@@ -54,38 +57,45 @@ public class BankService {
     }
 
     public ResponseEntity getTokenByPinOrFingerprint(String authenticationType, String pan, String pinOrFingerprint) throws HttpServerErrorException {
+        AuthType authType = AuthType.valueOf(authenticationType);
 
+        ResponseEntity result = null;
+
+        switch (authType){
+            case PIN:
+            case FINGERPRINT:
+                result = checkPinOrFingerprint(pan, pinOrFingerprint);
+                break;
+            default:
+                throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, applicationProperties.WRONG_AUTHENTICATION);
+        }
+
+        return result;
+    }
+
+    private ResponseEntity checkPinOrFingerprint(String pan, String pinOrFingerprint) throws HttpServerErrorException{
         Cards card = cardRepo.findByPan(pan);
 
-        if(authenticationType.equals("PIN") && card.getPin().equals(pinOrFingerprint)) {
-            String generatedToken = generateToken(card.getPin(), card.getPan());
+        if(card.getPin().equals(pinOrFingerprint) || card.getFingerprint().equals(pinOrFingerprint)) {
+            String generatedToken = generateToken(card.getPan());
             validatedCardStorage.putCard(card.getPan(), generatedToken);
 
             Map<String, String> param = new HashMap<>();
-            param.put("cardToken", generatedToken);
+            param.put(CARD_TOKEN, generatedToken);
 
             return new ResponseEntity(param, HttpStatus.OK);
-
-        }else if(authenticationType.equals("FINGERPRINT") && card.getFingerprint().equals(pinOrFingerprint)) {
-            String generatedToken = generateToken(card.getFingerprint(), card.getPan());
-            validatedCardStorage.putCard(card.getPan(), generatedToken);
-
-            Map<String, String> param = new HashMap<>();
-            param.put("cardToken", generatedToken);
-
-            return new ResponseEntity(param, HttpStatus.OK);
-
         }
 
         throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, applicationProperties.WRONG_AUTHENTICATION);
     }
+
 
     public ResponseEntity getBalance(String token) throws HttpServerErrorException {
         Cards card = getCardByToken(token);
 
         Integer balance =  card.getBalance();
 
-        if(validatedCardStorage.getValidatedCard().get(card.getPan())==null){
+        if(validatedCardStorage.getValidatedCard().get(card.getPan())== null){
             throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, applicationProperties.FAIL_GET_BALANCE);
         }
 
@@ -126,33 +136,32 @@ public class BankService {
     }
 
     private Cards getCardByToken(String token){
-        Algorithm algorithm = Algorithm.HMAC256("card");
+        Algorithm algorithm = Algorithm.HMAC256(CARD);
 
         JWTVerifier verifier = JWT.require(algorithm)
-                .withIssuer("Card")
+                .withIssuer(CARD_UPPERCASE)
                 .build();
         DecodedJWT decodedJWT = verifier.verify(token);
 
-        String pan = ((Claim) decodedJWT.getClaim("pan")).asString();
+        String pan = ((Claim) decodedJWT.getClaim(PAN)).asString();
 
         Cards card = cardRepo.findByPan(pan);
 
         return card;
     }
 
-    private String generateToken(String pin, String pan) {
-        Algorithm algorithm = Algorithm.HMAC256("card");
+    private String generateToken(String pan) {
+        Algorithm algorithm = Algorithm.HMAC256(CARD);
 
         Instant now = Instant.now();
         String jwtToken = JWT.create()
-                .withIssuer("Card")
-                .withSubject("Card Details")
-                .withClaim("pan", pan)
+                .withIssuer(CARD_UPPERCASE)
+                .withSubject(CARD_DETAILS)
+                .withClaim(PAN, pan)
                 .withIssuedAt(now)
                 .withExpiresAt(now.plusSeconds(120))
                 .sign(algorithm);
 
         return jwtToken;
-
     }
 }
